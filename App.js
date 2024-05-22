97ae32baffff9a549135432883c55687415e85b3
Nam <hoainam15112002@gmail.com>
2024-05-22 19:23:48 +0700

    some update

--- App.js
+++ App.js
@@ -12,18 +12,21 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Provider store={Store}>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Splash" component={SplashScreen}></Stack.Screen>
-          <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
-          <Stack.Screen name="Register" component={RegistrationScreen}></Stack.Screen>
-          <Stack.Screen name="Home" component={Test}></Stack.Screen>
-          {/* <Stack.Screen name="AddToChat" component={AddToChatScreen}></Stack.Screen> */}
-          <Stack.Screen name="ManageDevice" component={ManageDevice}></Stack.Screen>
-        </Stack.Navigator>
-      </Provider>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Provider store={Store}>
+    //     <Stack.Navigator screenOptions={{headerShown: false}}>
+    //       <Stack.Screen name="Splash" component={SplashScreen}></Stack.Screen>
+    //       <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
+    //       <Stack.Screen name="Register" component={RegistrationScreen}></Stack.Screen>
+    //       <Stack.Screen name="Home" component={Test}></Stack.Screen>
+    //       {/* <Stack.Screen name="AddToChat" component={AddToChatScreen}></Stack.Screen> */}
+    //       <Stack.Screen name="ManageDevice" component={ManageDevice}></Stack.Screen>
+    //     </Stack.Navigator>
+    //   </Provider>
+    // </NavigationContainer>
+    <View>
+      <Text>nam</Text>
+    </View>
   );
 };
 
